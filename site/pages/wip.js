48baeda1aa10806c798bb47d941862863a454a12
fquach <fionna.quach.821@gmail.com>
2023-11-21 20:45:49 -0800

    install sass?

--- site/pages/wip.js
+++ site/pages/wip.js
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home(){
 	return (
@@ -6,10 +8,44 @@ export default function Home(){
 		<div>
 			<Head>
 				<title>Home</title>
+				<link rel="icon" href="/ventiico.png" />
 			</Head>
+
+			<TopNav></TopNav>
 
 			<div>
 				<p>Hello World!</p>
+			</div>
+		</div>
+	);
+}
+
+function TopNav() {
+	return (
+		<div>
+			<Image
+				src="/ventiico.png"
+				width={100}
+				height={100}
+				alt='Placeholder'
+			/>
+
+			<div>
+				<btn>
+					<Link href={"/"}>Home</Link>
+				</btn>
+				<btn>
+					<Link href={"/"}>Arknights</Link>
+				</btn>
+				<btn>
+					<Link href={"/"}>Genshin</Link>
+				</btn>
+				<btn>
+					<Link href={"/"}>Star Rail</Link>
+				</btn>
+				<btn>
+					<Link href={"/"}>Webring</Link>
+				</btn>
 			</div>
 		</div>
 	);
