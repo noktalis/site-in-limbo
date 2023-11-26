c238364f4fcc0e53a59a2fd9fd10e5f96a72fef5
fquach <129032141+noktalis@users.noreply.github.com>
2023-11-26 14:46:37 -0800

    set new index page

--- site/pages/index.js
+++ site/pages/index.js
@@ -1,131 +1,56 @@
 import Head from 'next/head';
-import styles from '../styles/css/Home.module.css';
+import Layout from '../components/layout';
+import { ThemeContext } from '../components/ThemeContext';
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/ventiico.png" />
-      </Head>
+/**
+ * Home page of website
+ * @returns 
+ */
+export default function Home(){
+	return (
+		<ThemeContext.Provider value=''>
+			<Head>
+				<title>Home</title>
+				<link rel="icon" href="/ventiico.png" />
+			</Head>
+			<Layout><Content/></Layout>
+		</ThemeContext.Provider>
+	); // end return
+} // end Home Component
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="/wip">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  );
+/**
+ * Contents uniquely defined on each page
+ * @returns 
+ */
+function Content() {
+	return (
+		<div>
+			<h1>Header 1</h1>
+			<h6>Header 6</h6>
+			<p>Hello World!</p>
+			<hr></hr>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu nisl nunc mi ipsum faucibus vitae. Luctus accumsan tortor posuere ac ut consequat semper viverra nam. Et pharetra pharetra massa massa ultricies mi quis. Suspendisse potenti nullam ac tortor vitae purus. Libero nunc consequat interdum varius sit amet mattis.</p>
+			<ul>
+				<li>Item 2</li>
+				<li>Item 1</li>
+				<li>Item 4</li>
+				<li>Item 3</li>
+			</ul>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<p>hi</p>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<p>hi</p>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<p>hi</p>
+		</div>
+	);
 }
